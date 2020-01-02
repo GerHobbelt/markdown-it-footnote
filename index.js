ee4716a0098d2d010b273859041e30c7b45c03a2
Michael Mathews <micmath@gmail.com>
2020-01-02 17:06:11 +0000

    Adds support for footnote refs with text content

--- index.js
+++ index.js
@@ -18,12 +18,14 @@ function render_footnote_anchor_name(tokens, idx, options, env/*, slf*/) {
 
 function render_footnote_caption(tokens, idx/*, options, env, slf*/) {
   var n = Number(tokens[idx].meta.id + 1).toString();
-
   if (tokens[idx].meta.subId > 0) {
     n += ':' + tokens[idx].meta.subId;
   }
 
-  return '[' + n + ']';
+  var caption = new String('[' + n + ']');
+  if (tokens[idx].meta.text) { caption.text = tokens[idx].meta.text; }
+
+  return caption;
 }
 
 function render_footnote_ref(tokens, idx, options, env, slf) {
@@ -33,6 +35,10 @@ function render_footnote_ref(tokens, idx, options, env, slf) {
 
   if (tokens[idx].meta.subId > 0) {
     refid += ':' + tokens[idx].meta.subId;
+  }
+
+  if (caption.text) {
+    return '<a href="#fn' + id + '" id="fnref' + refid + '">' + caption.text + '<sup class="footnote-ref">' + caption + '</sup></a>';
   }
 
   return '<sup class="footnote-ref"><a href="#fn' + id + '" id="fnref' + refid + '">' + caption + '</a></sup>';
@@ -223,6 +229,64 @@ module.exports = function footnote_plugin(md) {
     return true;
   }
 
+  // Process footnote references with text ([^n ...])
+  function footnote_ref_with_text(state, silent) {
+    var label,
+        pos,
+        footnoteId,
+        footnoteSubId,
+        token,
+        max = state.posMax,
+        start = state.pos;
+
+    // should be at least 6 chars - "[^n x]"
+    if (start + 5 > max) { return false; }
+
+    if (!state.env.footnotes || !state.env.footnotes.refs) { return false; }
+    if (state.src.charCodeAt(start) !== 0x5B/* [ */) { return false; }
+    if (state.src.charCodeAt(start + 1) !== 0x5E/* ^ */) { return false; }
+
+    for (pos = start + 2; pos < max; pos++) {
+      if (state.src.charCodeAt(pos) === 0x0A /*linefeed */) { return false; }
+      if (state.src.charCodeAt(pos) === 0x5D /* ] */) {
+        break;
+      }
+    }
+
+    if (pos === start + 2) { return false; } // no empty footnote labels
+    if (pos >= max) { return false; }
+    pos++;
+
+    label = state.src.slice(start + 2, pos - 1);
+    if ( !label || !label.match(/^(\d+) (.+)/) ) { return false; }
+    label = RegExp.$1;
+    var text = RegExp.$2;
+    
+    if (typeof state.env.footnotes.refs[':' + label] === 'undefined') { return false; }
+
+    if (!silent) {
+      if (!state.env.footnotes.list) { state.env.footnotes.list = []; }
+
+      if (state.env.footnotes.refs[':' + label] < 0) {
+        footnoteId = state.env.footnotes.list.length;
+        state.env.footnotes.list[footnoteId] = { label: label, count: 0 };
+        state.env.footnotes.refs[':' + label] = footnoteId;
+      } else {
+        footnoteId = state.env.footnotes.refs[':' + label];
+      }
+
+      footnoteSubId = state.env.footnotes.list[footnoteId].count;
+      state.env.footnotes.list[footnoteId].count++;
+
+      token      = state.push('footnote_ref', '', 0);
+      token.meta = { id: footnoteId, subId: footnoteSubId, label: label, text: text };
+    }
+
+    state.pos = pos;
+    state.posMax = max;
+    return true;
+  }
+
   // Process footnote references ([^...])
   function footnote_ref(state, silent) {
     var label,
@@ -362,6 +426,7 @@ module.exports = function footnote_plugin(md) {
 
   md.block.ruler.before('reference', 'footnote_def', footnote_def, { alt: [ 'paragraph', 'reference' ] });
   md.inline.ruler.after('image', 'footnote_inline', footnote_inline);
-  md.inline.ruler.after('footnote_inline', 'footnote_ref', footnote_ref);
+  md.inline.ruler.after('footnote_inline', 'footnote_ref_with_text', footnote_ref_with_text);
+  md.inline.ruler.after('footnote_ref_with_text', 'footnote_ref', footnote_ref);
   md.core.ruler.after('inline', 'footnote_tail', footnote_tail);
 };
